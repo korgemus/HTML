4a5654fe6bfba36fdb9126c896ce7a93ab008f87
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-12-19 04:01:42 +0300

    push

--- Основа.js
+++ Основа.js
@@ -3,16 +3,23 @@ var button1 = document.getElementById('theme');
 var button2 = document.getElementById('images');
 var button3 = document.getElementById('main');
 var flag1 = 0;
+localStorage.setItem('theme',0);
+
+function setTheme(){
+  document.body.style.backgroundColor = localStorage.getItem('theme');
+}
 
 function changeColor(){
-  if(flag1 == 0){
-    document.body.style.backgroundColor = "#deb887";
+  if(localStorage.getItem('theme') == 0){
+    document.body.style.backgroundColor = "#deb887";    
     flag1 = 1; 
+    localStorage.setItem('theme',1);
   }
   else
   {
-    document.body.style.backgroundColor = "#aec3eb";
+    document.body.style.backgroundColor = "#aec3eb";    
     flag1 = 0; 
+    localStorage.setItem('theme',0);
   }
 }
 
